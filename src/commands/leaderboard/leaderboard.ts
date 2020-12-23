5af6c099686dc2ea1b6b5f8e2c93332bdbea80de
AsoDesu_ <AsoDesu_@users.noreply.github.com>
2020-12-23 20:08:01 +0000

    i haven't tested most of this so i hope it doesn't crash bot

--- src/commands/leaderboard/leaderboard.ts
+++ src/commands/leaderboard/leaderboard.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed } from 'discord.js'
+import { Message, MessageEmbed, MessageReaction, User } from 'discord.js'
 import randColor from '../../randomColor'
 
 import getUserFromScoresaber from '../../scoresaberApiGrabber'
@@ -15,25 +15,81 @@ import '../../firebaseinnit'
 var db = firebase.firestore()
 
 type leaderboardUser = {
-    scoresaber: string, 
-    name: string, 
+    scoresaber: string,
+    name: string,
     pp: number,
     discord: string
 }
 
 // I have further proof that array's in JavaScript are nightmares that are only created to make the coder suffer eternally until they eventually give up and find some hacky solution
 
-async function command(msg: Message, args: string[]) {
+async function command(msg: Message, args: string[], reqPage?: number) {
+    // Test if the command was last run 2 minuites ago
     var rateLimitTest = rateLimit(msg, 120000)
     if (rateLimitTest.rateLimit && !msg.member.hasPermission('MANAGE_ROLES')) {
         msg.reply(`Hey not to fast, Try again in ${formatNumber.msToMandS(rateLimitTest.againIn)}s`)
         return
     }
 
+    // Generate the array
     msg.channel.startTyping()
-    var leaderboard: leaderboardUser[] = []
+    var page = reqPage ? reqPage : 1
+    var lbArr = (await createLeaderboardArray(page))
+    var leaderboard = lbArr.lb
 
-    // bad code, very very bad code, see above statement
+    // Add the array to the embed description
+    var leaderboardMsg: string = ""
+    leaderboard.forEach(item => {
+        leaderboardMsg = leaderboardMsg.concat(`#${(leaderboard.indexOf(item)) + page * 10 - 10 + 1} - \`${item.name}\` - ${formatNumber.numberWithCommas(item.pp)}pp \n`)
+    })
+
+    // Give the role to the #1 player
+    //giveRoleTo1(msg, leaderboard[0].discord)
+
+    // Send the message
+    var sentMsg = await msg.channel.send(new MessageEmbed({
+        "title": "NO Clan Leaderboard",
+        "description": leaderboardMsg,
+        "color": randColor(),
+        "footer": {
+            "text": `Page ${page}/${Math.ceil(lbArr.totalLength / 10)}`
+        }
+    }))
+
+    // Stop typing if the message didn't
+    msg.channel.stopTyping(true)
+    sentMsg.react(reqPage ? '⬅️' : '⏺️')
+    sentMsg.react('➡️')
+
+    // Reaction filter (Only the two emotes and only the user who did the command)
+    const filter = (reaction: MessageReaction, user: User) => {
+        return (reaction.emoji.name === '⬅️') || (reaction.emoji.name === '➡️') && !user.bot
+    }
+
+    // Create collector
+    var rpCollector = sentMsg.createReactionCollector(filter, { time: 60000 })
+
+    // Someone reacted
+    rpCollector.on('collect', (reaction: MessageReaction, user: User) => {
+        // Ignore the bots own reactions
+        if (user.bot) return 
+        // If the reaction is previous page and they are not on the first page
+        if (reaction.emoji.name == '⬅️' && !(page == 1)) {
+            command(msg, args, page - 1)
+            rpCollector.stop()
+            return;
+        // If the reaction is next page, and they are not on the last page
+        } else if (reaction.emoji.name == '➡️' && !(page == lbArr.totalLength)) {
+            command(msg,args, page + 1)
+            rpCollector.stop()
+            return
+        }
+    })
+}
+
+// Function to generate the leaderboard array
+async function createLeaderboardArray(page: number) {
+    var leaderboard: leaderboardUser[] = []
     var userCollection = (await db.collection('users').get()).docs
     for (const doc of userCollection) {
         var userData = doc.data()
@@ -41,24 +97,14 @@ async function command(msg: Message, args: string[]) {
         var user: leaderboardUser = { scoresaber: userData.scoresaberId, name: ssData.playerInfo.playerName, pp: ssData.playerInfo.pp, discord: doc.id }
         leaderboard.push(user)
     }
-
     leaderboard = leaderboard.sort(function (a, b) {
         return b.pp - a.pp
     })
 
-    var leaderboardMsg: string = ""
-    leaderboard.forEach(item => {
-        leaderboardMsg = leaderboardMsg.concat(`#${leaderboard.indexOf(item) + 1} - \`${item.name}\` - ${formatNumber.numberWithCommas(item.pp)}pp \n`)
-    })
-
-    giveRoleTo1(msg, leaderboard[0].discord)
-    
-    msg.channel.send(new MessageEmbed({
-        "title": "NO Clan Leaderboard",
-        "description": leaderboardMsg,
-        "color": randColor()
-    }))
-    msg.channel.stopTyping(true)
+    return {
+        totalLength: leaderboard.length,
+        lb: leaderboard.splice(((page * 10) - 10), 10)
+    }
 }
 
 export default command
