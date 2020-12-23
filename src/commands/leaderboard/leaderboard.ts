92c3733c7c03706c4ac83f993b127559c7f5c0b5
AsoDesu_ <AsoDesu_@users.noreply.github.com>
2020-12-23 21:09:40 +0000

    leaderboard page loading is now actually good

--- src/commands/leaderboard/leaderboard.ts
+++ src/commands/leaderboard/leaderboard.ts
@@ -23,42 +23,24 @@ type leaderboardUser = {
 
 // I have further proof that array's in JavaScript are nightmares that are only created to make the coder suffer eternally until they eventually give up and find some hacky solution
 
-async function command(msg: Message, args: string[], reqPage?: number) {
+async function command(msg: Message, args: string[]) {
     // Test if the command was last run 2 minuites ago
-    /*var rateLimitTest = rateLimit(msg, 120000)
+    var rateLimitTest = rateLimit(msg, 120000)
     if (rateLimitTest.rateLimit && !msg.member.hasPermission('MANAGE_ROLES')) {
         msg.reply(`Hey not to fast, Try again in ${formatNumber.msToMandS(rateLimitTest.againIn)}s`)
         return
-    } */
+    }
 
     // Generate the array
     msg.channel.startTyping()
-    var page = reqPage ? reqPage : 1
-    var lbArr = (await createLeaderboardArray(page))
-    var leaderboard = lbArr.lb
 
-    // Add the array to the embed description
-    var leaderboardMsg: string = ""
-    leaderboard.forEach(item => {
-        leaderboardMsg = leaderboardMsg.concat(`#${(leaderboard.indexOf(item)) + page * 10 - 10 + 1} - \`${item.name}\` - ${formatNumber.numberWithCommas(item.pp)}pp \n`)
-    })
-
-    // Give the role to the #1 player
-    //giveRoleTo1(msg, leaderboard[0].discord)
-
-    // Send the message
-    var sentMsg = await msg.channel.send(new MessageEmbed({
-        "title": "NO Clan Leaderboard",
-        "description": leaderboardMsg,
-        "color": randColor(),
-        "footer": {
-            "text": `Page ${page}/${Math.ceil(lbArr.totalLength / 10)}`
-        }
-    }))
+    var page = 1
+    var leaderboard = (await createLeaderboardArray(page))
+    var sentMsg = await msg.channel.send(createEmbedFromLbArray(leaderboard, msg, page))
 
     // Stop typing if the message didn't
     msg.channel.stopTyping(true)
-    sentMsg.react(reqPage ? '⬅️' : '⏺️')
+    sentMsg.react('⬅️')
     sentMsg.react('➡️')
 
     // Reaction filter (Only the two emotes and only the user who did the command)
@@ -72,19 +54,54 @@ async function command(msg: Message, args: string[], reqPage?: number) {
     // Someone reacted
     rpCollector.on('collect', (reaction: MessageReaction, user: User) => {
         // Ignore the bots own reactions
-        if (user.bot) return 
+        if (user.bot) return
+
         // If the reaction is previous page and they are not on the first page
         if (reaction.emoji.name == '⬅️' && !(page == 1)) {
-            command(msg, args, page - 1)
-            rpCollector.stop()
+            page = page - 1
+            sentMsg.edit(createEmbedFromLbArray(leaderboard, msg, page))
+
+            reaction.users.remove(user.id)
+
             return;
-        // If the reaction is next page, and they are not on the last page
-        } else if (reaction.emoji.name == '➡️' && !(page == lbArr.totalLength)) {
-            command(msg,args, page + 1)
-            rpCollector.stop()
+            // If the reaction is next page, and they are not on the last page
+        } else if (reaction.emoji.name == '➡️' && !(page == leaderboard.length)) {
+            page = page + 1
+            sentMsg.edit(createEmbedFromLbArray(leaderboard, msg, page))
+
+            reaction.users.remove(user.id)
+
             return
         }
     })
+}
+
+function createEmbedFromLbArray(leaderboard: leaderboardUser[], msg: Message, page: number) {
+    var leaderboardArrClone = [...leaderboard]
+    var newLb = getItems(leaderboardArrClone, page)
+
+    // Add the array to the embed description
+    var leaderboardMsg: string = ""
+    newLb.forEach(item => {
+        leaderboardMsg = leaderboardMsg.concat(`#${(newLb.indexOf(item)) + page * 10 - 10 + 1} - \`${item.name}\` - ${formatNumber.numberWithCommas(item.pp)}pp \n`)
+    })
+
+    // Give the role to the #1 player
+    //giveRoleTo1(msg, leaderboard[0].discord)
+
+    // Send the message
+    return new MessageEmbed({
+        "title": "NO Clan Leaderboard",
+        "description": leaderboardMsg,
+        "color": randColor(),
+        "footer": {
+            "text": `Page ${page}/${Math.ceil(leaderboard.length / 10)}`
+        }
+    })
+}
+
+function getItems(leaderboard: leaderboardUser[], page: number) {
+    return leaderboard.splice(((page * 10) - 10), 10)
 }
 
 // Function to generate the leaderboard array
@@ -101,10 +118,8 @@ async function createLeaderboardArray(page: number) {
         return b.pp - a.pp
     })
 
-    return {
-        totalLength: leaderboard.length,
-        lb: leaderboard.splice(((page * 10) - 10), 10)
-    }
+    return leaderboard
 }
+// leaderboard.splice(((page * 10) - 10), 10)
 
 export default command
