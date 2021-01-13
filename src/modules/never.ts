ac5da9ec7d6812bc54fa44cda1b0f1c49f7ee4d3
StormPacer <73640025+StormPacer@users.noreply.github.com>
2021-01-13 16:20:53 +0100

    Updated never.ts

--- src/modules/never.ts
+++ src/modules/never.ts
@@ -1,15 +1,12 @@
 import { Message } from "discord.js";
 
-var responses = [
-    "gonna give you up, never gonna let you down, never gonna run around and desert you"
-]
+
 
 function never(msg: Message) {
 
-var never =(Math.floor(Math.random()*10)+1);
-if (never == 1)
-{
-    msg.channel.send("gonna give you up, never gonna let you down, never gonna run around and desert you")
-}
+    var never =(Math.floor(Math.random()*10)+1);
+    if (never == 1) {
+        msg.channel.send("gonna give you up, never gonna let you down, never gonna run around and desert you")
+    }
 }
 export default never
