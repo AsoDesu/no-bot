501c310f599846497b4abc530eff30f95870a065
AsoDesu <53526710+AsoDesu@users.noreply.github.com>
2021-05-16 10:48:28 +0100

    many many changes: you can now get the user of someone who hasn't linked their scoresaber, you can now marry people, you can now run >neko and >neko lewd in dm with no limits, you can now link things without needing a scoresaber

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,12 @@ import collect from "./commands/economy/uncollectedxp/collect";
 import collectCache from "./commands/economy/uncollectedxp/cache";
 import shop from "./commands/economy/shop/shop";
 
+// Marriage
+import propose from "./commands/marriage/propose";
+import accept from "./commands/marriage/accept";
+import deny from "./commands/marriage/deny";
+import divorce from "./commands/marriage/divorce";
+
 // Moduels
 import BOT from "./modules/@bot";
 import vc from "./modules/vc";
@@ -46,191 +52,193 @@ import botLog from "./modules/botLog";
 import lbCache from "./commands/leaderboard/cache";
 import autoupdate from "./modules/updateLeaderboardCache";
 
+// Commands that can be used in dm
+var dmCommandWhitelist = ["accept", "neko"];
+
 client.on("message", async (msg: Discord.Message) => {
-  if (msg.author.bot) return;
+	if (msg.author.bot) return;
 
-  if (
-    msg.content == "devvcj" &&
-    !msg.guild &&
-    msg.author.id == "580425653325791272"
-  ) {
-    vc.join(client);
-    return;
-  }
-  if (
-    msg.content == "devvcd" &&
-    !msg.guild &&
-    msg.author.id == "580425653325791272"
-  ) {
-    vc.leave(client);
-    return;
-  }
-  if (
-    msg.content.startsWith("*") &&
-    !msg.guild &&
-    msg.author.id == "580425653325791272"
-  ) {
-    thoughts(client, msg.content.replace("*", ""));
-    return;
-  }
+	if (msg.content == "devvcj" && !msg.guild && msg.author.id == "580425653325791272") {
+		vc.join(client);
+		return;
+	}
+	if (msg.content == "devvcd" && !msg.guild && msg.author.id == "580425653325791272") {
+		vc.leave(client);
+		return;
+	}
+	if (msg.content.startsWith("*") && !msg.guild && msg.author.id == "580425653325791272") {
+		thoughts(client, msg.content.replace("*", ""));
+		return;
+	}
 
-  if (!msg.guild) {
-    return;
-  }
-  if (msg.member.roles.cache.find((r) => r.name == "dumb-bot-ban")) return;
-  collectCache.addxp(msg.author.id, 0.5);
+	const args = msg.content.toLowerCase().slice(process.env.PREFIX.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-  if (!msg.content.startsWith(process.env.PREFIX)) {
-    checkForMemes(msg);
-    return;
-  }
+	if (!msg.guild && !dmCommandWhitelist.includes(command)) {
+		return;
+	}
 
-  const args = msg.content
-    .toLowerCase()
-    .slice(process.env.PREFIX.length)
-    .split(/ +/);
-  const command = args.shift().toLowerCase();
-  switch (command) {
-    case "help":
-      HELP(msg, args);
-      return;
-    case "add":
-      addUser(msg, args);
-      return;
-    case "remove":
-      removeUser(msg, args);
-      return;
-    case "user":
-      getUser(msg, args);
-      return;
-    case "adduser":
-      addModUser(msg, args);
-      return;
-    case "leaderboard":
-      leaderboard(msg, args);
-      return;
-    case "link":
-      link(msg, args);
-      return;
-    case "color":
-      colour(msg, args);
-      return;
-    case "dev":
-      dev(msg, args);
-      return;
-    case "pfp":
-      pfp(msg, args);
-      return;
-    case "addmod":
-      addMod(msg, args);
-      return;
-    case "scuffed":
-      scuffed(msg, args);
-      return;
-    case "play":
-      getxp(msg, args);
-      return;
-    case "bal":
-      bal(msg, args);
-      return;
-    case "baltop":
-      baltop(msg, args);
-      return;
-    case "levelup":
-      levelup(msg, args);
-      return;
-    case "gamble":
-      gamble(msg, args);
-      return;
-    case "addxp":
-      modxp.giveXp(msg, args);
-      return;
-    case "removexp":
-      modxp.removeXp(msg, args);
-      return;
-    case "setxp":
-      modxp.setXp(msg, args);
-      return;
-    case "togglegamble":
-      modxp.toggleGamble(msg, args);
-      return;
-    case "setjackpot":
-      modxp.setJackpot(msg, args);
-      return;
-    case "collect":
-      collect(msg, args);
-      return;
-    case "shop":
-      shop.shopcmd(msg, args);
-      return;
-    case "buy":
-      shop.buycmd(msg, args);
-      return;
-    case "inv":
-      shop.invcmd(msg, args);
-      return;
-    case "use":
-      shop.usecmd(msg, args);
-      return;
-    case "resetxp232":
-      modxp.resetXp(msg, args);
-      return;
-    case "rawuserdata":
-      rawuserdata(msg, args);
-      return;
-    case "shorten":
-      shortener.create(msg);
-      return;
-    case "geturl":
-      shortener.get(msg);
-      return;
-    case "delurl":
-      shortener.del(msg);
-      return;
-    case "neko":
-      neko(msg, args);
-      return;
-    case "createrr":
-      createRR(msg, args);
-      return;
-    case "removerr":
-      removeRR(msg, args);
-      return;
-    case "eval":
-      evalCmd(msg, args);
-      return;
-  }
-  // case '': ; return;
-  alises(command, msg, args);
+	if (msg.guild && msg.member.roles.cache.find((r) => r.name == "dumb-bot-ban")) return;
+	collectCache.addxp(msg.author.id, 0.5);
+
+	if (!msg.content.startsWith(process.env.PREFIX)) {
+		checkForMemes(msg);
+		return;
+	}
+
+	switch (command) {
+		case "help":
+			HELP(msg, args);
+			return;
+		case "add":
+			addUser(msg, args);
+			return;
+		case "remove":
+			removeUser(msg, args);
+			return;
+		case "user":
+			getUser(msg, args);
+			return;
+		case "adduser":
+			addModUser(msg, args);
+			return;
+		case "leaderboard":
+			leaderboard(msg, args);
+			return;
+		case "link":
+			link(msg, args);
+			return;
+		case "color":
+			colour(msg, args);
+			return;
+		case "dev":
+			dev(msg, args);
+			return;
+		case "pfp":
+			pfp(msg, args);
+			return;
+		case "addmod":
+			addMod(msg, args);
+			return;
+		case "scuffed":
+			scuffed(msg, args);
+			return;
+		case "play":
+			getxp(msg, args);
+			return;
+		case "bal":
+			bal(msg, args);
+			return;
+		case "baltop":
+			baltop(msg, args);
+			return;
+		case "levelup":
+			levelup(msg, args);
+			return;
+		case "gamble":
+			gamble(msg, args);
+			return;
+		case "addxp":
+			modxp.giveXp(msg, args);
+			return;
+		case "removexp":
+			modxp.removeXp(msg, args);
+			return;
+		case "setxp":
+			modxp.setXp(msg, args);
+			return;
+		case "togglegamble":
+			modxp.toggleGamble(msg, args);
+			return;
+		case "setjackpot":
+			modxp.setJackpot(msg, args);
+			return;
+		case "collect":
+			collect(msg, args);
+			return;
+		case "shop":
+			shop.shopcmd(msg, args);
+			return;
+		case "buy":
+			shop.buycmd(msg, args);
+			return;
+		case "inv":
+			shop.invcmd(msg, args);
+			return;
+		case "use":
+			shop.usecmd(msg, args);
+			return;
+		case "resetxp232":
+			modxp.resetXp(msg, args);
+			return;
+		case "rawuserdata":
+			rawuserdata(msg, args);
+			return;
+		case "shorten":
+			shortener.create(msg);
+			return;
+		case "geturl":
+			shortener.get(msg);
+			return;
+		case "delurl":
+			shortener.del(msg);
+			return;
+		case "neko":
+			neko(msg, args);
+			return;
+		case "createrr":
+			createRR(msg, args);
+			return;
+		case "removerr":
+			removeRR(msg, args);
+			return;
+		case "eval":
+			evalCmd(msg, args);
+			return;
+		case "marry":
+			propose(msg, args);
+			return;
+		case "accept":
+			accept(msg, args);
+			return;
+		case "deny":
+			deny(msg, args);
+			return;
+		case "divorce":
+			divorce(msg, args);
+			return;
+	}
+	// case '': ; return;
+	alises(command, msg, args);
 });
 
 function checkForMemes(msg: Discord.Message) {
-  // memes
-  if (msg.content.includes("777284907302912000")) {
-    BOT(msg);
-    return;
-  }
-  if (msg.content.includes("never")) {
-    never(msg);
-    return;
-  }
+	// memes
+	if (msg.content.includes("777284907302912000")) {
+		BOT(msg);
+		return;
+	}
+	if (msg.content.includes("never")) {
+		never(msg);
+		return;
+	}
 }
 
 function getClient() {
-  return client;
+	return client;
 }
 
 client.on("ready", () => {
-  console.log("Connected to Discord");
-  botLog("Connected to discord", client, __filename);
-  client.user.setActivity("Version 1.7.1");
+	console.log("Connected to Discord");
+	botLog("Connected to discord", client, __filename);
+	client.user.setActivity("Version 1.8.0");
 
-  lbCache.updateCache(client);
+	lbCache.updateCache(client);
 
-  if (autoupdate.task) {
-    autoupdate.stopCron();
-  }
-  autoupdate.startCron();
+	if (autoupdate.task) {
+		autoupdate.stopCron();
+	}
+	autoupdate.startCron();
 });
 
 client.on("guildMemberAdd", userJoin);
@@ -238,7 +246,7 @@ client.on("guildMemberAdd", userJoin);
 client.on("guildMemberRemove", userLeave);
 
 client.on("raw", (d) => {
-  packetRecived(d, client);
+	packetRecived(d, client);
 });
 
 client.login(process.env.TOKEN);
@@ -251,15 +259,15 @@ const app = Express();
 app.use(Express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Aso was ere!");
+	res.send("Aso was ere!");
 });
 
 app.get("/:shorturl", (req, res) => {
-  shortener.red(req, res);
+	shortener.red(req, res);
 });
 
 app.listen(process.env.PORT);
 
 export default {
-  getClient,
+	getClient,
 };
